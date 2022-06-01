c310ccaf6efcd1540f60707df0b4fcb7324eca19
João Vitor <jv.zaniolo@gmail.com>
2022-06-01 15:52:06 -0300

    Change id to slug

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,22 +1,22 @@
 import Link from 'next/link'
-import { Flex, Heading, Img, Stack, Text } from '@chakra-ui/react'
+import { Flex, Heading, Img, Text } from '@chakra-ui/react'
 import type { Item as ItemProps } from '~/types/item'
 
-export function Item({ id, name, description, price, image }: ItemProps) {
+export function Item({ slug, name, description, price, image }: ItemProps) {
   return (
-    <Link href={`/items/${id}`} passHref>
-      <Flex as="a" direction="column">
-        <Img src={image} alt={name} w="64" borderTopRadius="md" />
-        <Stack shadow="md" p="3" borderBottomRadius="md">
+    <Link href={`/items/${slug}`} passHref>
+      <Flex as="a" direction="column" shadow="md" maxW="72">
+        <Img src={image} alt={name} w="72" borderTopRadius="md" />
+        <Flex p="3" gap="3" flex="1" direction="column" borderBottomRadius="md">
           <Heading size="sm">{name}</Heading>
-          <Text>{description}</Text>
-          <Text>
+          <Text noOfLines={3}>{description}</Text>
+          <Text mt="auto" fontWeight="bold">
             {new Intl.NumberFormat('en-US', {
               style: 'currency',
               currency: 'USD',
             }).format(price)}
           </Text>
-        </Stack>
+        </Flex>
       </Flex>
     </Link>
   )
