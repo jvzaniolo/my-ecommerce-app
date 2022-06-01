52dee86bc6eff0582fcf6aacd99cb4a027f2592e
João Vitor <jv.zaniolo@gmail.com>
2022-06-01 15:52:22 -0300

    Remove link to home on header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { MdOutlineShoppingCart } from 'react-icons/md'
-import { Box, Button, Flex, Icon, IconButton } from '@chakra-ui/react'
+import { Box, Flex, Heading, Icon, IconButton } from '@chakra-ui/react'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,23 +13,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
           align="center"
           justify="space-between"
         >
-          <Link href="/" passHref>
-            <Button as="a" variant="ghost">
-              My E-Commerce App
-            </Button>
-          </Link>
+          <Heading size="sm">My E-Commerce App</Heading>
 
-          <Flex align="center">
-            <Link href="/cart" passHref>
-              <IconButton
-                as="a"
-                icon={<Icon as={MdOutlineShoppingCart} />}
-                aria-label="cart"
-              />
-            </Link>
-          </Flex>
+          <Link href="/cart" passHref>
+            <IconButton
+              as="a"
+              aria-label="cart"
+              icon={<Icon as={MdOutlineShoppingCart} />}
+            />
+          </Link>
         </Flex>
       </Flex>
+
       <Box as="main" p="4" h="full" w="container.xl" mx="auto">
         {children}
       </Box>
