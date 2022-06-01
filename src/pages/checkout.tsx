906baa977d46430600a776dc65ae47bfd841adbb
João Vitor <jv.zaniolo@gmail.com>
2022-06-01 12:15:39 -0300

    Add Checkout page

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Heading } from '@chakra-ui/react'
+
+const Checkout: NextPage = () => {
+  return (
+    <div>
+      <Heading size="lg">Checkout</Heading>
+    </div>
+  )
+}
+
+export default Checkout
