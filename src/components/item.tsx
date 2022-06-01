11eca26938c2a46eeec109707eb30cfc5fca7f83
João Vitor <jv.zaniolo@gmail.com>
2022-06-01 12:14:45 -0300

    Add container.xl around layout

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,21 +1,23 @@
 import Link from 'next/link'
-import { Heading, Img, Stack, Text } from '@chakra-ui/react'
+import { Flex, Heading, Img, Stack, Text } from '@chakra-ui/react'
 import type { Item as ItemProps } from '~/types/item'
 
 export function Item({ id, name, description, price, image }: ItemProps) {
   return (
     <Link href={`/items/${id}`} passHref>
-      <Stack as="a">
-        <Img src={image} alt={name} w="64" />
-        <Heading size="sm">{name}</Heading>
-        <Text>{description}</Text>
-        <Text>
-          {new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          }).format(price)}
-        </Text>
-      </Stack>
+      <Flex as="a" direction="column">
+        <Img src={image} alt={name} w="64" borderTopRadius="md" />
+        <Stack shadow="md" p="3" borderBottomRadius="md">
+          <Heading size="sm">{name}</Heading>
+          <Text>{description}</Text>
+          <Text>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(price)}
+          </Text>
+        </Stack>
+      </Flex>
     </Link>
   )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,31 +5,32 @@ import { Box, Button, Flex, Icon, IconButton } from '@chakra-ui/react'
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Flex h="100vh" direction="column">
-      <Flex
-        as="header"
-        p="4"
-        h="14"
-        shadow="base"
-        align="center"
-        justify="space-between"
-      >
-        <Link href="/" passHref>
-          <Button as="a" variant="ghost">
-            My E-Commerce App
-          </Button>
-        </Link>
-
-        <Flex align="center">
-          <Link href="/cart" passHref>
-            <IconButton
-              as="a"
-              icon={<Icon as={MdOutlineShoppingCart} />}
-              aria-label="cart"
-            />
+      <Flex as="header" shadow="base">
+        <Flex
+          p="3"
+          w="container.xl"
+          mx="auto"
+          align="center"
+          justify="space-between"
+        >
+          <Link href="/" passHref>
+            <Button as="a" variant="ghost">
+              My E-Commerce App
+            </Button>
           </Link>
+
+          <Flex align="center">
+            <Link href="/cart" passHref>
+              <IconButton
+                as="a"
+                icon={<Icon as={MdOutlineShoppingCart} />}
+                aria-label="cart"
+              />
+            </Link>
+          </Flex>
         </Flex>
       </Flex>
-      <Box as="main" p="4" h="full">
+      <Box as="main" p="4" h="full" w="container.xl" mx="auto">
         {children}
       </Box>
     </Flex>
