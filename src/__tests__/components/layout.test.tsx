c75b90406ba1c3530f4682e05a0becc880627074
João Vitor <jv.zaniolo@gmail.com>
2022-06-01 18:29:35 -0300

    test(Layout): add initial test

---
+++ src/__tests__/components/layout.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import { Layout } from '~/components/layout'
+
+describe('Layout', () => {
+  it('should render the Layout and its content', () => {
+    const { container } = render(<Layout>test</Layout>)
+
+    expect(container).toMatchSnapshot()
+  })
+})
