c5a08572a217b008068892c59bedf4b1bc2d6848
João Vitor <jv.zaniolo@gmail.com>
2022-06-02 11:20:56 -0300

    Add images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['static.cdnlive.com.br', 'picsum.photos'],
+  },
 }
 
 module.exports = nextConfig
