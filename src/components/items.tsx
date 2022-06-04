2e2b43bb7e902db43532d2c163c6443d317ee593
João Vitor <jv.zaniolo@gmail.com>
2022-06-04 11:41:58 -0300

    Remove fetcher

--- src/components/items.tsx
+++ src/components/items.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import useSWR from 'swr'
 import { Flex, Heading, Img, Text } from '@chakra-ui/react'
-import { fetcher } from '~/services/axios'
 import { toUSCurrency } from '~/utils/format'
+
 import type { Item as ItemType } from '~/types/item'
 
 export function Items() {
-  const { data: items } = useSWR<ItemType[]>('/items', fetcher)
+  const { data: items } = useSWR<ItemType[]>('/items')
 
   return (
     <Flex gap="4" flexWrap="wrap">
@@ -14,7 +14,12 @@ export function Items() {
         items?.map(item => (
           <Link key={item.id} href={`/items/${item.slug}`} passHref>
             <Flex as="a" direction="column" shadow="md" maxW="72">
-              <Img src={item.image} alt={item.name} w="72" borderTopRadius="md" />
+              <Img
+                src={item.image}
+                alt={item.name}
+                w="72"
+                borderTopRadius="md"
+              />
               <Flex
                 p="3"
                 gap="3"
