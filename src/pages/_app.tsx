3949e3e9a13648ff65e91262ed260f1fb90eb500
João Vitor <jv.zaniolo@gmail.com>
2022-06-04 19:40:02 -0300

    Remove global SWR config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from 'next/app'
 
-import { SWRConfig } from 'swr'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '~/components/layout'
-import { fetcher } from '~/services/axios'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <Layout>
-        <SWRConfig value={{ fetcher }}>
-          <Component {...pageProps} />
-        </SWRConfig>
+        <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
   )
