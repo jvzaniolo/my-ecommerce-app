0b0ccea871158d6fd81d9d2c13ab28a456306015
João Vitor <jv.zaniolo@gmail.com>
2022-06-04 11:42:25 -0300

    Add global fetcher SWR config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
-import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Layout } from '~/components/layout'
+import { fetcher } from '~/services/axios'
+
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Layout>
-        <Component {...pageProps} />
+        <SWRConfig value={{ fetcher }}>
+          <Component {...pageProps} />
+        </SWRConfig>
       </Layout>
     </ChakraProvider>
   )
