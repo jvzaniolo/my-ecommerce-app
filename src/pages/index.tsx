903dc2e893864d5127061813529711bd9fec05bb
João Vitor <jv.zaniolo@gmail.com>
2022-06-04 19:39:20 -0300

    Use SWR fallback option instead of SWRConfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,48 @@
 import type { GetServerSideProps, NextPage } from 'next'
+import type { Item } from '~/types/item'
 import type { Fallback } from '~/types/swr'
 
-import { SWRConfig } from 'swr'
-import api from '~/services/axios'
-import Items from '~/components/items'
+import Link from 'next/link'
+import useSWR from 'swr'
+import { Flex, Heading, Img, Text } from '@chakra-ui/react'
+import api, { fetcher } from '~/services/axios'
+import { toUSCurrency } from '~/utils/format'
 
-const HomePage: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
+const Home: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
+  const { data: items } = useSWR<Item[]>('/items', fetcher, fallback)
+
   return (
-    <SWRConfig value={{ fallback }}>
-      <Items />
-    </SWRConfig>
+    <Flex gap="4" flexWrap="wrap">
+      {items && items.length > 0 ? (
+        items?.map(item => (
+          <Link key={item.id} href={`/items/${item.slug}`} passHref>
+            <Flex as="a" direction="column" shadow="md" maxW="72">
+              <Img
+                src={item.image}
+                alt={item.name}
+                w="72"
+                borderTopRadius="md"
+              />
+              <Flex
+                p="3"
+                gap="3"
+                flex="1"
+                direction="column"
+                borderBottomRadius="md"
+              >
+                <Heading size="sm">{item.name}</Heading>
+                <Text noOfLines={3}>{item.description}</Text>
+                <Text mt="auto" fontWeight="bold">
+                  {toUSCurrency(item.price)}
+                </Text>
+              </Flex>
+            </Flex>
+          </Link>
+        ))
+      ) : (
+        <Text>There are no items yet.</Text>
+      )}
+    </Flex>
   )
 }
 
@@ -25,4 +58,4 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 }
 
-export default HomePage
+export default Home
