768a3524e3aa70c0712b606c479290b78332c578
João Vitor <jv.zaniolo@gmail.com>
2022-06-06 11:58:38 -0300

    Add Shipping, Billing and Payment forms

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,27 +1,87 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import type { Item } from '~/types/item'
+import type { Fallback } from '~/types/swr'
+import type { FormData } from '~/types/checkout-form'
 
-import { useSWRConfig } from 'swr'
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import useSWR from 'swr'
+import { type SubmitHandler, useForm } from 'react-hook-form'
+import { Box, Button, Checkbox, Flex, Heading, Stack } from '@chakra-ui/react'
+import api, { fetcher } from '~/services/axios'
+import Input from '~/components/input'
 import OrderSummary from '~/components/order-summary'
+import ShippingForm from '~/components/shipping-form'
 
-const Checkout: NextPage = () => {
-  const { cache } = useSWRConfig()
+const Checkout: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
+  const { data: cart } = useSWR<Item[]>('/cart', fetcher, { fallback })
+  const { handleSubmit, register } = useForm<FormData>()
 
-  const cart = cache.get('/cart')
-
-  console.log({ cart })
+  const onPurchase: SubmitHandler<FormData> = data => {
+    console.log(data)
+  }
 
   return (
-    <>
-      <Heading size="lg">Checkout</Heading>
+    <Box w="full">
+      <Heading as="h1" size="xl">
+        Checkout
+      </Heading>
 
-      <Flex>
-        <Box flex="2">Hello</Box>
+      <Flex
+        as="form"
+        mt="4"
+        gap="10"
+        direction={['column', 'column', 'row']}
+        onSubmit={handleSubmit(onPurchase)}
+      >
+        <Flex flex="2" gap="6" direction="column">
+          <Flex as="section" direction="column">
+            <ShippingForm register={register} />
+          </Flex>
 
-        <OrderSummary />
+          <Flex as="section" direction="column">
+            <Heading as="h2" size="lg">
+              Billing
+            </Heading>
+
+            <Flex direction="row" mt="3">
+              <Checkbox defaultChecked={true}>Use same as Shipping</Checkbox>
+            </Flex>
+          </Flex>
+
+          <Flex as="section" direction="column">
+            <Heading as="h2" size="lg">
+              Payment
+            </Heading>
+
+            <Flex mt="3" direction="column">
+              <Input label="Card Number" {...register('payment.cardNumber')} />
+              <Stack direction="row">
+                <Input label="Expiry" {...register('payment.expiry')} />
+                <Input label="CVV" {...register('payment.cvv')} />
+              </Stack>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Box h="sm" flex="1" pos="sticky" top="20">
+          <OrderSummary cartItems={cart}>
+            <Button type="submit">Purchase</Button>
+          </OrderSummary>
+        </Box>
       </Flex>
-    </>
+    </Box>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await api.get('/cart')
+
+  return {
+    props: {
+      fallback: {
+        '/cart': data,
+      },
+    },
+  }
 }
 
 export default Checkout
