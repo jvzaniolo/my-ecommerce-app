bd9b3cb27b9c5617425ad1bf3739dc63e8cef797
João Vitor <jv.zaniolo@gmail.com>
2022-06-14 15:15:58 -0300

    feat: add sign-in page

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,104 @@
+import type { NextPage } from 'next'
+
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  Button,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  useToast,
+} from '@chakra-ui/react'
+import Input from '~/components/input'
+import { supabase } from '~/services/supabase'
+
+type SignUpFormData = {
+  email: string
+  password: string
+}
+
+const signUpSchema = yup.object({
+  email: yup.string().email('Email is invalid').required('Email is required'),
+  password: yup.string().required('Password is required'),
+})
+
+const SignIn: NextPage = () => {
+  const {
+    register,
+    setFocus,
+    resetField,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    resolver: yupResolver(signUpSchema),
+  })
+  const toast = useToast()
+  const router = useRouter()
+
+  const onSignIn: SubmitHandler<SignUpFormData> = async data => {
+    const { email, password } = data
+    const { error } = await supabase.auth.signIn({ email, password })
+
+    if (error) {
+      resetField('password')
+      setFocus('password')
+
+      toast({
+        title: 'Sign In',
+        description: error.message,
+        status: 'error',
+        duration: 5000,
+      })
+    } else {
+      router.push('/')
+    }
+  }
+
+  return (
+    <Center>
+      <Container maxW="container.sm">
+        <Heading mb="6">Sign In</Heading>
+
+        <Flex
+          direction="column"
+          gap="3"
+          as="form"
+          onSubmit={handleSubmit(onSignIn)}
+        >
+          <Input
+            label="E-mail"
+            type="email"
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <Input
+            label="Password"
+            type="password"
+            {...register('password')}
+            error={errors.password?.message}
+          />
+
+          <Button
+            mt="6"
+            w="full"
+            type="submit"
+            colorScheme="purple"
+            isLoading={isSubmitting}
+          >
+            Sign In
+          </Button>
+
+          <Link href="/sign-up" passHref>
+            <Button as="a">Don&apos;t have an account yet? Sign up here</Button>
+          </Link>
+        </Flex>
+      </Container>
+    </Center>
+  )
+}
+
+export default SignIn
