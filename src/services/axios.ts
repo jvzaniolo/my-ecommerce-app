5451f18c9eed2a9db8ae58ffa11a216a51a687ad
João Vitor <jv.zaniolo@gmail.com>
2022-06-13 22:47:25 -0300

    fix: set axios to point to the app url and use next.js api routes

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3333' })
+const api = axios.create({ baseURL: 'http://localhost:3000' })
 const fetcher = (url: string) => api.get(url).then(res => res.data)
 
 export { fetcher }
