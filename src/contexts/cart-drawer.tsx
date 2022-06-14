c997445a441af983351b6027b07b50d6757bd133
João Vitor <jv.zaniolo@gmail.com>
2022-06-13 21:18:39 -0300

    feat: add cart-drawer context

---
+++ src/contexts/cart-drawer.tsx
@@ -0,0 +1,40 @@
+import { useDisclosure } from '@chakra-ui/react'
+import { createContext, type ReactNode, useContext } from 'react'
+import CartDrawer from '~/components/cart-drawer'
+
+type CartDrawerContextValue = {
+  isCartDrawerOpen: boolean
+  onOpenCartDrawer: () => void
+  onCloseCartDrawer: () => void
+}
+
+const CartDrawerContext = createContext<CartDrawerContextValue | null>(null)
+
+function CartDrawerProvider({ children }: { children: ReactNode }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const value = {
+    isCartDrawerOpen: isOpen,
+    onOpenCartDrawer: onOpen,
+    onCloseCartDrawer: onClose,
+  }
+
+  return (
+    <CartDrawerContext.Provider value={value}>
+      {children}
+      <CartDrawer isOpen={isOpen} onClose={onClose} />
+    </CartDrawerContext.Provider>
+  )
+}
+
+function useCartDrawer() {
+  const context = useContext(CartDrawerContext)
+
+  if (!context) {
+    throw new Error('useCartDrawer must be used within a CartDrawerProvider')
+  }
+
+  return context
+}
+
+export { CartDrawerProvider, useCartDrawer }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '~/components/layout'
+import { CartDrawerProvider } from '~/contexts/cart-drawer'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CartDrawerProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartDrawerProvider>
     </ChakraProvider>
   )
 }
