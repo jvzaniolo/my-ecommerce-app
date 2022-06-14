a0c548434194cc09826ad72f4e5958dfb7aae8ab
João Vitor <jv.zaniolo@gmail.com>
2022-06-14 15:15:46 -0300

    feat: add user context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '~/components/layout'
 import { CartDrawerProvider } from '~/contexts/cart-drawer'
+import { UserProvider } from '~/contexts/user'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <CartDrawerProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <UserProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UserProvider>
       </CartDrawerProvider>
     </ChakraProvider>
   )
