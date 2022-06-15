ea4dbc37534411611cd5aa31aee24ab13175fb17
João Vitor <jv.zaniolo@gmail.com>
2022-06-15 15:11:12 -0300

    feat: migrate cart page to supabase usage

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -20,15 +20,21 @@ import { toUSCurrency } from '~/utils/format'
 import Quantity from '~/components/quantity'
 import OrderSummary from '~/components/order-summary'
 
+type Cart = {
+  id: string
+  quantity: number
+  product: Item
+}
+
 /**
  * @deprecated
  * @see Using CartDrawer for now
  * @see src/components/cart-drawer.tsx
  */
 const Cart: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
-  const { data: cart } = useSWR<Item[]>(
+  const { data: cart } = useSWR<Cart[]>(
     '/cart',
-    () => fetcher('http://localhost:3333/cart'),
+    () => fetcher('http://localhost:3000/api/cart'),
     { fallback }
   )
 
@@ -42,7 +48,7 @@ const Cart: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
     mutate(
       '/cart',
       async () => {
-        await fetcher(`http://localhost/cart/${id}`, {
+        await fetcher(`http://localhost:3000/api/cart/${id}`, {
           method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
@@ -105,29 +111,31 @@ const Cart: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
                   <Img
                     as={Image}
                     layout="fill"
-                    src={item.image}
-                    alt={item.name}
+                    src={item.product.image}
+                    alt={item.product.name}
                     borderLeftRadius="md"
                   />
                 </Box>
                 <Flex p="4" h="full" direction="column" gap="2" flex="2">
-                  <NextLink href={`/items/${item.slug}`}>
+                  <NextLink href={`/items/${item.product.slug}`}>
                     <Link size="md" fontWeight="bold" fontSize="xl">
-                      {item.name}
+                      {item.product.name}
                     </Link>
                   </NextLink>
 
-                  <Text noOfLines={3} title={item.description}>
-                    {item.description}
+                  <Text noOfLines={3} title={item.product.description}>
+                    {item.product.description}
                   </Text>
                   <Quantity
                     value={item.quantity}
                     onChange={value => onUpdateItemQuantity(item.id, value)}
-                    max={item.stock}
+                    max={item.product.stock}
                   />
 
                   <Flex justify="space-between" mt="auto">
-                    <Text fontWeight="bold">{toUSCurrency(item.price)}</Text>
+                    <Text fontWeight="bold">
+                      {toUSCurrency(item.product.price)}
+                    </Text>
                     <Button
                       variant="link"
                       onClick={() => onRemoveCartItem(item.id)}
@@ -144,13 +152,13 @@ const Cart: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
         </Stack>
 
         <Box h="sm" flex="1" pos="sticky" top="20">
-          <OrderSummary cartItems={cart}>
+          {/* <OrderSummary cartItems={cart}>
             <NextLink href="/checkout" passHref>
               <Button mt="auto" as="a" w="full">
                 Checkout
               </Button>
             </NextLink>
-          </OrderSummary>
+          </OrderSummary> */}
         </Box>
       </Flex>
     </Box>
@@ -158,7 +166,7 @@ const Cart: NextPage<{ fallback: Fallback }> = ({ fallback }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await fetcher('http://localhost:3333/cart')
+  const data = await fetcher('http://localhost:3000/api/cart')
 
   return {
     props: {
