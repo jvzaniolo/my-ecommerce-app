64bfae5a0fc4220d9e2301dd9abb132429fee487
João Vitor <jv.zaniolo@gmail.com>
2022-06-16 21:13:47 -0300

    fix: [slug] product api

--- src/pages/api/products/[slug].ts
+++ src/pages/api/products/[slug].ts
@@ -2,28 +2,20 @@ import type { NextApiHandler } from 'next'
 import { supabase } from '~/services/supabase'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { slug } = req.query
+  const slug = req.query.slug as string
 
-  switch (req.method) {
-    case 'GET': {
-      const { data, error, status } = await supabase
-        .from('product')
-        .select('*')
-        .eq('slug', slug)
-        .single()
+  if (req.method === 'GET') {
+    const { data, error, status } = await supabase
+      .from('product')
+      .select('*')
+      .match({ slug })
+      .single()
 
-      if (error) {
-        return res.status(status).json({ message: error.message })
-      }
-
-      return res.status(status).json(data)
-    }
-    default: {
-      return res.status(405).json({
-        message: 'Method Not Allowed',
-      })
-    }
+    return res.status(status).json(error || data)
   }
+
+  res.setHeader('Allow', ['GET'])
+  return res.status(405).json({ message: 'Method Not Allowed' })
 }
 
 export default handler
