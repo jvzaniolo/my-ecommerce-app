bc434ed4027f89168a1590a87c2c0b4f1c668c76
João Vitor <jv.zaniolo@gmail.com>
2022-06-17 19:10:39 -0300

    fix: import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '~/components/layout'
-import { CartDrawerProvider } from '~/contexts/cart-drawer'
 import { UserProvider } from '~/contexts/user'
+import { CartDrawerProvider } from '~/contexts/cart-drawer'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
