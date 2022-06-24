fbdec23579c6ea4da1282a169e89734272979746
João Vitor <jv.zaniolo@gmail.com>
2022-06-24 18:57:53 -0300

    feat: add SWR fetcher

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,3 +3,5 @@ import { default as _axios } from 'axios'
 export const axios = _axios.create({
   baseURL: process.env.BASE_URL || 'http://localhost:3000',
 })
+
+export const fetcher = (url: string) => axios.get(url).then(res => res.data)
