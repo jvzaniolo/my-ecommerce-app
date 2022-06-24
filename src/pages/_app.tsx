75b2d85a612f1c37d987314698fd79050d30ada3
João Vitor <jv.zaniolo@gmail.com>
2022-06-24 18:59:23 -0300

    fix: remove initial SWR config as it was not working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import { BareFetcher, SWRConfig } from 'swr'
 import { Layout } from '~/components/layout'
 import { CartDrawerProvider } from '~/contexts/cart-drawer'
 import { UserProvider } from '~/contexts/user'
-import { axios } from '~/services/axios'
-
-const fetcher: BareFetcher = resource =>
-  axios.get(resource).then(res => res.data())
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -15,9 +10,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <CartDrawerProvider>
         <UserProvider>
           <Layout>
-            <SWRConfig value={{ fetcher }}>
-              <Component {...pageProps} />
-            </SWRConfig>
+            <Component {...pageProps} />
           </Layout>
         </UserProvider>
       </CartDrawerProvider>
