7e4e1f5cf021bb585c7a3de6d41ec84b6178aa91
João Vitor <jv.zaniolo@gmail.com>
2022-06-24 18:27:49 -0300

    feat: update item if it already exists on cart

--- src/pages/api/cart/index.ts
+++ src/pages/api/cart/index.ts
@@ -26,17 +26,53 @@ const handler: NextApiHandler = async (req, res) => {
     // TODO: allow anonymous user to add items to cart
     if (!user) return res.status(401).json('Unauthorized')
 
-    const { data: cartId } = await supabase
+    const {
+      data: cart,
+      status: cartStatus,
+      error: cartError,
+    } = await supabase
       .from('cart')
       .select('id')
       .match({ user_id: user.id })
       .single()
 
+    if (cartError) return res.status(cartStatus).json(cartError)
+
+    // Check for existing cart item
+    const {
+      data: cartItem,
+      status: cartItemStatus,
+      error: cartItemError,
+    } = await supabase
+      .from('cart_item')
+      .select('id, quantity')
+      .match({ product_id: productId })
+
+    if (cartItemError) return res.status(cartItemStatus).json(cartItemError)
+
+    // If cart item exists, update the quantity with the new quantity
+    if (cartItem.length) {
+      const {
+        data: updatedCartItem,
+        status: updatedCartItemStatus,
+        error: updatedCartItemError,
+      } = await supabase
+        .from('cart_item')
+        .update({ quantity: cartItem[0].quantity + quantity })
+        .match({ product_id: productId })
+        .single()
+
+      return res
+        .status(updatedCartItemStatus)
+        .json(updatedCartItemError || updatedCartItem)
+    }
+
+    // Or else, create a new cart item
     const { data, error, status } = await supabase
       .from('cart_item')
       .insert({
         quantity,
-        cart_id: cartId,
+        cart_id: cart.id,
         user_id: user.id,
         product_id: productId,
       })
