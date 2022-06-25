620ea7a157936b0a0354c5d4bfc3ad2c37f23b84
João Vitor <jv.zaniolo@gmail.com>
2022-06-25 20:12:55 -0300

    fix: display cartDrawer with empty cart

--- src/components/cart-drawer.tsx
+++ src/components/cart-drawer.tsx
@@ -37,93 +37,85 @@ export const CartDrawer: FC<CartDrawerProps> = ({ isOpen, onClose }) => {
   const { onCloseCartDrawer } = useCartDrawer()
   const router = useRouter()
 
-  if (cart) {
-    const cartTotal = cart.items.reduce((acc: any, item: any) => {
-      return acc + item.product.price * item.quantity
-    }, 0)
-
-    return (
-      <Drawer
-        id="cart"
-        size="sm"
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader shadow="md">Cart</DrawerHeader>
-
-          <DrawerBody>
-            <Stack spacing="8">
-              {cart.items.length > 0 ? (
-                cart.items.map((item: any) => (
-                  <Flex key={item.id} gap="3">
-                    <AspectRatio ratio={1} flex="1">
-                      <Img
-                        as={Image}
-                        src={item.product.image}
-                        alt={item.product.name}
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </AspectRatio>
-                    <Flex flex="2" direction="column" justify="space-between">
-                      <Heading as="h3" size="md" fontWeight="medium">
-                        {item.product.name}
-                      </Heading>
-                      <Flex justify="space-between">
-                        <Text fontSize="lg" fontWeight="semibold">
-                          {toUSCurrency(item.product.price * item.quantity)}
-                        </Text>
-                        <Button
-                          variant="link"
-                          onClick={() => {
-                            optimisticDeleteItem(cart, item.id)
-                          }}
-                        >
-                          Remove
-                        </Button>
-                      </Flex>
-                      <Quantity
-                        max={item.product.stock}
-                        value={item.quantity}
-                        onChange={value => {
-                          optimisticUpdateItemQuantity(cart, item.id, value)
-                        }}
-                      />
-                    </Flex>
-                  </Flex>
-                ))
-              ) : (
-                <Text>Your cart is empty.</Text>
-              )}
-            </Stack>
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Link href="/checkout" passHref>
-              <Button
-                as="a"
-                flex="1"
-                colorScheme="purple"
-                onClick={event => {
-                  event.preventDefault()
-                  onCloseCartDrawer()
-                  router.push('/checkout')
-                }}
-              >
-                Checkout: {toUSCurrency(cartTotal)}
-              </Button>
-            </Link>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    )
-  }
+  const cartTotal = cart
+    ? cart.items.reduce((acc: any, item: any) => {
+        return acc + item.product.price * item.quantity
+      }, 0)
+    : 0
 
   if (error) return <>{error.message}</>
 
-  return <>Loading...</>
+  return (
+    <Drawer size="sm" isOpen={isOpen} placement="right" onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader shadow="md">Cart</DrawerHeader>
+
+        <DrawerBody>
+          <Stack spacing="8">
+            {cart && cart.items.length > 0 ? (
+              cart.items.map((item: any) => (
+                <Flex key={item.id} gap="3">
+                  <AspectRatio ratio={1} flex="1">
+                    <Img
+                      as={Image}
+                      src={item.product.image}
+                      alt={item.product.name}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </AspectRatio>
+                  <Flex flex="2" direction="column" justify="space-between">
+                    <Heading as="h3" size="md" fontWeight="medium">
+                      {item.product.name}
+                    </Heading>
+                    <Flex justify="space-between">
+                      <Text fontSize="lg" fontWeight="semibold">
+                        {toUSCurrency(item.product.price * item.quantity)}
+                      </Text>
+                      <Button
+                        variant="link"
+                        onClick={() => {
+                          optimisticDeleteItem(cart, item.id)
+                        }}
+                      >
+                        Remove
+                      </Button>
+                    </Flex>
+                    <Quantity
+                      max={item.product.stock}
+                      value={item.quantity}
+                      onChange={value => {
+                        optimisticUpdateItemQuantity(cart, item.id, value)
+                      }}
+                    />
+                  </Flex>
+                </Flex>
+              ))
+            ) : (
+              <Text>Your cart is empty.</Text>
+            )}
+          </Stack>
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Link href="/checkout" passHref>
+            <Button
+              as="a"
+              flex="1"
+              colorScheme="purple"
+              onClick={event => {
+                event.preventDefault()
+                onCloseCartDrawer()
+                router.push('/checkout')
+              }}
+            >
+              Checkout: {toUSCurrency(cartTotal)}
+            </Button>
+          </Link>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
 }
