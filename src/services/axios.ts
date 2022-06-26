3ac6726d56fdbc7855a48ae961a1c379f7bec19e
João Vitor <jv.zaniolo@gmail.com>
2022-06-26 18:42:58 -0300

    fix: axios API_URL on prod

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,11 +3,7 @@ import { AxiosResponse, default as _axios } from 'axios'
 type Fetcher = <T>(url: string) => Promise<AxiosResponse<T>['data']>
 
 export const axios = _axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
 })
 
-export const fetcher: Fetcher = url =>
-  axios.get(url).then(res => {
-    console.log(url)
-    return res.data
-  })
+export const fetcher: Fetcher = url => axios.get(url).then(res => res.data)
