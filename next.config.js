505251fa7c02e4d5c74e3c74922acaf9932ae160
João Vitor <jv.zaniolo@gmail.com>
2022-06-26 19:53:31 -0300

    fix: host name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['https://eemxsmzdpiifqmddaxdl.supabase.co'],
+    domains: ['eemxsmzdpiifqmddaxdl.supabase.co'],
   },
 }
 
