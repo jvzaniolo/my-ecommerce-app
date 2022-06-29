ffcb26c0a5af6f0ea82e79e8f7de26231b2b9eeb
João Vitor <jv.zaniolo@gmail.com>
2022-06-29 19:51:13 -0300

    feat: migrate to prisma

--- src/server/routers/order.ts
+++ src/server/routers/order.ts
@@ -1,31 +1,30 @@
 import { z } from 'zod'
-import { Order } from '~/types'
 import { Context } from '../context'
 import { createRouter } from '../createRouter'
-import { supabase } from '../supabase'
+import { prisma } from '../prisma'
 
 export const orderRouter = createRouter()
   .query('all', {
-    async resolve() {
-      const { data } = await supabase
-        .from<Order>('order')
-        .select('*, user(*), items:order_item(*, product(*))')
+    async resolve({ ctx }) {
+      const { user } = ctx as Context
 
-      return data
+      return await prisma.order.findFirst({
+        where: { userId: user.id },
+        include: { user: true, orderItems: { include: { product: true } } },
+      })
     },
   })
   .query('byId', {
     input: z.object({
       id: z.string(),
     }),
-    async resolve({ input }) {
-      const { data } = await supabase
-        .from<Order>('order')
-        .select('*, user(*), items:order_item(*, product(*))')
-        .match({ id: input.id })
-        .single()
+    async resolve({ input, ctx }) {
+      const { user } = ctx as Context
 
-      return data
+      return await prisma.order.findFirst({
+        where: { userId: user.id, id: input.id },
+        include: { user: true, orderItems: { include: { product: true } } },
+      })
     },
   })
   .mutation('create', {
@@ -33,7 +32,7 @@ export const orderRouter = createRouter()
       cart: z.object({
         items: z.array(
           z.object({
-            product_id: z.string(),
+            productId: z.string(),
             quantity: z.number(),
           })
         ),
@@ -42,28 +41,19 @@ export const orderRouter = createRouter()
     async resolve({ input, ctx }) {
       const { user } = ctx as Context
 
-      const orderRes = await supabase
-        .from('order')
-        .insert([
-          {
-            user_id: user?.id,
+      if (!user.id) throw new Error('User not found')
+
+      return await prisma.order.create({
+        data: {
+          userId: user.id,
+          orderItems: {
+            create: input.cart.items.map(i => ({
+              productId: i.productId,
+              quantity: i.quantity,
+            })),
           },
-        ])
-        .select('*, user(*)')
-        .single()
-
-      const orderItemRes = await supabase
-        .from('order_item')
-        .insert([
-          ...input.cart.items.map(item => ({
-            order_id: orderRes.data.id,
-            product_id: item.product_id,
-            quantity: item.quantity,
-            user_id: user?.id,
-          })),
-        ])
-        .select('*, product(*)')
-
-      return { ...orderRes.data, items: orderItemRes.data } as Order
+        },
+        include: { user: true, orderItems: { include: { product: true } } },
+      })
     },
   })
