7bbfb02dbcb67ffb0d538693bbedce3740cd44d7
João Vitor <jv.zaniolo@gmail.com>
2022-07-01 22:35:42 -0300

    chore: misc

--- src/components/cart-drawer.tsx
+++ src/components/cart-drawer.tsx
@@ -25,12 +25,74 @@ import { toUSCurrency } from '~/utils/format'
 import { trpc } from '~/utils/trpc'
 import { Quantity } from './quantity'
 
-type CartDrawerProps = {
-  isOpen: boolean
-  onClose: () => void
+type Cart = InferQueryOutput<'cart.all'>
+
+const useRemoveCartItemMutation = () => {
+  const utils = trpc.useContext()
+
+  return trpc.useMutation(['cart.remove'], {
+    onMutate({ itemId }) {
+      utils.cancelQuery(['cart.all'])
+
+      const prevCart = utils.getQueryData(['cart.all'])
+
+      utils.setQueryData(['cart.all'], old => {
+        return {
+          ...old,
+          items: old?.items.filter(i => i.id !== itemId),
+        } as Cart
+      })
+
+      return { prevCart }
+    },
+    onError: (_, __, context) => {
+      utils.setQueryData(['cart.all'], context?.prevCart as Cart)
+    },
+    onSettled: () => {
+      utils.invalidateQueries(['cart.all'])
+    },
+  })
 }
 
-export const CartDrawer: FC<CartDrawerProps> = ({ isOpen, onClose }) => {
+const useUpdateCartItemMutation = () => {
+  const utils = trpc.useContext()
+
+  return trpc.useMutation(['cart.update-quantity'], {
+    onMutate({ itemId, quantity }) {
+      utils.cancelQuery(['cart.all'])
+
+      const prevCart = utils.getQueryData(['cart.all'])
+
+      utils.setQueryData(['cart.all'], old => {
+        return {
+          ...old,
+          items: old?.items.map(i => {
+            if (i.id === itemId) {
+              return {
+                ...i,
+                quantity,
+              }
+            }
+            return i
+          }),
+        } as Cart
+      })
+
+      return { prevCart }
+    },
+    onError: (_, __, context) => {
+      utils.setQueryData(['cart.all'], context?.prevCart as Cart)
+    },
+    onSettled: () => {
+      utils.invalidateQueries(['cart.all'])
+    },
+  })
+}
+
+export const CartDrawer: FC<{
+  isOpen: boolean
+  onClose: () => void
+}> = ({ isOpen, onClose }) => {
   const router = useRouter()
   const { session } = useUser()
   const { onCloseCartDrawer } = useCartDrawer()
@@ -143,68 +205,4 @@ export const CartDrawer: FC<CartDrawerProps> = ({ isOpen, onClose }) => {
       </DrawerContent>
     </Drawer>
   )
-}
-
-type Cart = InferQueryOutput<'cart.all'>
-
-const useRemoveCartItemMutation = () => {
-  const utils = trpc.useContext()
-
-  return trpc.useMutation(['cart.remove'], {
-    onMutate({ itemId }) {
-      utils.cancelQuery(['cart.all'])
-
-      const prevCart = utils.getQueryData(['cart.all'])
-
-      utils.setQueryData(['cart.all'], old => {
-        return {
-          ...old,
-          items: old?.items.filter(i => i.id !== itemId),
-        } as Cart
-      })
-
-      return { prevCart }
-    },
-    onError: (_, __, context) => {
-      utils.setQueryData(['cart.all'], context?.prevCart as Cart)
-    },
-    onSettled: () => {
-      utils.invalidateQueries(['cart.all'])
-    },
-  })
-}
-
-const useUpdateCartItemMutation = () => {
-  const utils = trpc.useContext()
-
-  return trpc.useMutation(['cart.update-quantity'], {
-    onMutate({ itemId, quantity }) {
-      utils.cancelQuery(['cart.all'])
-
-      const prevCart = utils.getQueryData(['cart.all'])
-
-      utils.setQueryData(['cart.all'], old => {
-        return {
-          ...old,
-          items: old?.items.map(i => {
-            if (i.id === itemId) {
-              return {
-                ...i,
-                quantity,
-              }
-            }
-            return i
-          }),
-        } as Cart
-      })
-
-      return { prevCart }
-    },
-    onError: (_, __, context) => {
-      utils.setQueryData(['cart.all'], context?.prevCart as Cart)
-    },
-    onSettled: () => {
-      utils.invalidateQueries(['cart.all'])
-    },
-  })
 }
