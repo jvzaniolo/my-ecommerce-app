fe06e7ad823ed97f203f230024665716b371cfb8
João Vitor <jv.zaniolo@gmail.com>
2022-07-01 22:45:21 -0300

    feat: create user router (signIn/signUp)

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,3 +1,62 @@
+import { z } from 'zod'
 import { createRouter } from '../createRouter'
+import { prisma } from '../prisma'
+import { supabase } from '../supabase'
 
 export const userRouter = createRouter()
+  .mutation('signIn', {
+    input: z.object({
+      email: z.string().email(),
+      password: z.string(),
+    }),
+
+    async resolve({ input }) {
+      const {
+        session,
+        user: authUser,
+        error,
+      } = await supabase.auth.signIn({
+        email: input.email,
+        password: input.password,
+      })
+
+      if (error) throw new Error(error.message)
+
+      const user = await prisma.user.findUnique({
+        where: { id: authUser?.id },
+      })
+
+      return { session, user }
+    },
+  })
+  .mutation('create', {
+    input: z.object({
+      email: z.string().email(),
+      password: z.string(),
+    }),
+
+    async resolve({ input }) {
+      const {
+        session,
+        user: authUser,
+        error,
+      } = await supabase.auth.signUp({
+        email: input.email,
+        password: input.password,
+      })
+
+      if (error) throw new Error(error.message)
+
+      const user = await prisma.user.create({
+        data: {
+          id: authUser?.id,
+          email: input.email,
+          cart: {
+            create: {},
+          },
+        },
+      })
+
+      return { session, user }
+    },
+  })
