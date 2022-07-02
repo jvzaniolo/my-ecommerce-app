3f87b2a968048b8114dc3cbfec0a272287b2bb10
João Vitor <jv.zaniolo@gmail.com>
2022-07-01 22:41:09 -0300

    fix: did not need that lol

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,24 +1,9 @@
 import { NextApiHandler } from 'next'
-import { prisma } from '~/server/prisma'
 import { supabase } from '~/server/supabase'
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'POST') {
-    supabase.auth.api.setAuthCookie(req, res)
-
-    const { user } = await supabase.auth.api.getUserByCookie(req)
-
-    if (!user) throw new Error('User not found')
-
-    await prisma.user.create({
-      data: {
-        id: user.id,
-        email: user.email as string,
-        cart: {
-          create: {},
-        },
-      },
-    })
+    return supabase.auth.api.setAuthCookie(req, res)
   }
 
   if (req.method === 'DELETE') {
